3a335781c102b5cb9d571d2288fbbc91cb726380
wisekey <bekov.tembulat.g@gmail.com>
2025-07-28 14:06:42 +0300

    feat: Add curtom Manager

--- mysite/blog/models.py
+++ mysite/blog/models.py
@@ -2,6 +2,13 @@ from django.db import models
 from django.utils import timezone
 from django.contrib.auth.models import User
 
+
+class PublishedManager(models.Manager):
+    def get_queryset(self):
+        return super().get_queryset() \
+            .filter(status=Post.Status.PUBLISHED)
+
+
 class Post(models.Model):
     class Status(models.TextChoices):
         DRAFT = 'DF', 'Draft'
@@ -37,6 +44,8 @@ class Post(models.Model):
         choices=Status.choices,
         default=Status.DRAFT
     )
+    objects = models.Manager()
+    published = PublishedManager()
 
     class Meta:
         ordering = ['-publish']
