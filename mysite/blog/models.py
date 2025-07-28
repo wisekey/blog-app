ec22fe2e9eb2707e5920d3f1cc744091252963bd
wisekey <bekov.tembulat.g@gmail.com>
2025-07-28 10:22:16 +0300

    feat: Add Post model

--- mysite/blog/models.py
+++ mysite/blog/models.py
@@ -1,1 +1,16 @@
 from django.db import models
+
+
+class Post(models.Model):
+    title = models.CharField(
+        max_length=250
+    )
+    slug = models.SlugField(
+        max_length=250,
+        unique=True,
+        db_index=True
+    )
+    body = models.TextField()
+
+    def __str__(self):
+        return self.title
