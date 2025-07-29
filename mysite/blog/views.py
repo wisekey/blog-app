d886ddccaae75ee3948b4e4d9b1e8f9fb7fb0b66
wisekey <bekov.tembulat.g@gmail.com>
2025-07-29 10:50:18 +0300

    add email share supporting

--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -1,21 +1,14 @@
 from django.shortcuts import render, get_object_or_404
+from django.views.generic import ListView
 from .models import Post
-from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
+from .forms import EmailPostForm
 
 
-def post_list(request):
-    post_list = Post.published.all()
-    paginator = Paginator(post_list, 3)
-    page_number = request.GET.get('page', 1)
-    posts = paginator.get_page(page_number)
-    
-    return render(
-        request,
-        "blog/post/list.html",
-        {
-            "posts": posts
-        }
-    )
+class PostListView(ListView):
+    queryset = Post.published.all()
+    context_object_name = 'posts'
+    paginate_by = 3
+    template_name = 'blog/post/list.html'
 
 
 def post_detail(request, year, month, day, post):
@@ -33,3 +26,24 @@ def post_detail(request, year, month, day, post):
         'blog/post/detail.html',
         {'post': post}
     )
+
+
+def post_shate(request, post_id):
+    post = get_object_or_404(
+        Post,
+        id=post_id,
+        status=Post.Status.PUBLISHED
+    )
+    if request.method == 'POST':
+        form = EmailPostForm(request.POST)
+        if form.is_valid():
+            cd = form.cleaned_data
+    else:
+        form = EmailPostForm
+    
+    data = {
+        'post': post,
+        'form': form
+    }
+
+    return render(request, 'blog/post/share.html', data)
