1f783a72e9b2ce2308cfac00638a539049265724
wisekey <bekov.tembulat.g@gmail.com>
2025-07-29 11:50:54 +0300

    sopports email

--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -2,7 +2,8 @@ from django.shortcuts import render, get_object_or_404
 from django.views.generic import ListView
 from .models import Post
 from .forms import EmailPostForm
-
+from django.core.mail import send_mail
+from django.conf import settings
 
 class PostListView(ListView):
     queryset = Post.published.all()
@@ -28,22 +29,39 @@ def post_detail(request, year, month, day, post):
     )
 
 
-def post_shate(request, post_id):
+def post_share(request, post_id):
     post = get_object_or_404(
         Post,
         id=post_id,
         status=Post.Status.PUBLISHED
     )
+
+    sent = False
+
     if request.method == 'POST':
         form = EmailPostForm(request.POST)
         if form.is_valid():
             cd = form.cleaned_data
+            post_url = request.build_absolute_uri(
+                post.get_absolute_url()
+            )
+            subject = f'{cd['name']} recommends you read {post.title}'
+            message = f'Read {post.title} at {post_url}\n\n' \
+                      f'{cd['name']}\'s ({cd['email']}) comments: {cd['comments']}'
+            send_mail(
+                subject,
+                message,
+                settings.EMAIL_HOST_USER,
+                [cd['to']]
+            )
+            sent = True
     else:
         form = EmailPostForm
     
     data = {
         'post': post,
-        'form': form
+        'form': form,
+        'sent': sent,
     }
 
     return render(request, 'blog/post/share.html', data)
--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -121,3 +121,15 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.yandex.ru'
+EMAIL_PORT = 465
+EMAIL_USE_SSL = True
+
+EMAIL_HOST_USER = 'tembulatbekoff@yandex.ru'
+EMAIL_HOST_PASSWORD = 'caitmlqeivnipavc'
+
+DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
+SERVER_EMAIL = EMAIL_HOST_USER
+EMAIL_ADMIN = EMAIL_HOST_USER
