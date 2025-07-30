8386bdf9a47fb80c70b5ab80c100ae36bb51335d
wisekey <bekov.tembulat.g@gmail.com>
2025-07-30 12:08:57 +0300

    add support tags, filter

--- mysite/blog/models.py
+++ mysite/blog/models.py
@@ -2,6 +2,7 @@ from django.db import models
 from django.utils import timezone
 from django.contrib.auth.models import User
 from django.urls import reverse
+from taggit.managers import TaggableManager
 
 
 class PublishedManager(models.Manager):
@@ -46,6 +47,8 @@ class Post(models.Model):
         choices=Status.choices,
         default=Status.DRAFT
     )
+    tags = TaggableManager()
+
     objects = models.Manager()
     published = PublishedManager()
 
--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -5,13 +5,28 @@ from .forms import EmailPostForm, CommentForm
 from django.core.mail import send_mail
 from django.conf import settings
 from django.views.decorators.http import require_POST
+from taggit.models import Tag
+from django.core.paginator import Paginator
+from taggit.models import Tag
+from django.db.models import Count
 
+def post_list(request, tag_slug=None):
+    post_list = Post.published.all()
 
-class PostListView(ListView):
-    queryset = Post.published.all()
-    context_object_name = 'posts'
-    paginate_by = 3
-    template_name = 'blog/post/list.html'
+    tag = None
+    if tag_slug:
+        tag = get_object_or_404(Tag, slug=tag_slug)
+        post_list = post_list.filter(tags__in=[tag])
+
+    paginator = Paginator(post_list, 3)
+    page_number = request.GET.get('page', 1)
+
+    posts = paginator.get_page(page_number)
+    
+    return render(request,
+                  'blog/post/list.html',
+                  {'posts': posts,
+                   'tag': tag})    
 
 
 def post_detail(request, year, month, day, post):
@@ -23,11 +38,31 @@ def post_detail(request, year, month, day, post):
         publish__month=month,
         publish__day=day
     )
+
+    comment_order = 'created'
+    if 'desc' in request.GET:
+        comment_order = '-created'
     
+    comments = post.comments.filter(active=True).order_by(comment_order)
+    
+    form = CommentForm()
+    
+    post_tags_ids = post.tags.values_list('id', flat=True)
+    similar_posts = Post.published.filter(tags__in=post_tags_ids) \
+        .exclude(id=post.id)
+    similar_posts = similar_posts.annotate(same_tags=Count('tags')) \
+        .order_by('-same_tags', '-publish')
+
     return render(
         request,
         'blog/post/detail.html',
-        {'post': post}
+        {
+            'post': post,
+            'comments': comments,
+            'form': form,
+            'similar_posts': similar_posts,
+            
+        }
     )
 
 
