d6aeae8f978100aa466752c0e57abd14225b689c
wisekey <bekov.tembulat.g@gmail.com>
2025-08-05 20:48:00 +0300

    refactor: use ModelSerializer

--- mysite/blog_api/serializers.py
+++ mysite/blog_api/serializers.py
@@ -3,16 +3,7 @@ from typing import Any
 from blog.models import Comment
 
 
-class CommentSerializer(serializers.Serializer):
-    name = serializers.CharField()
-    email = serializers.EmailField()
-    body = serializers.CharField()
-
-    def create(self, validated_date: dict[str, Any]) -> Comment:
-        return Comment(**validated_date)
-
-    def update(self, instance: Comment, validated_data: dict[str, Any]) -> Comment:
-        instance.name = validated_data.get("name", instance.name)
-        instance.email = validated_data.get("email", instance.email)
-        instance.body == validated_data.get("body", instance.body)
-        return instance
+class CommentSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Comment
+        fields = ["id", "author", "title", "body", "created"]
